adfa44203505806574bb5c6a8510660e84dd7467
AlexFirst <141620329+AlexFirst404@users.noreply.github.com>
2025-06-06 10:28:43 +0300

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+alias clr="clear"
+alias g="git"
+alias n="nano"
+alias xlog='grep "(EE)\|(WW)\|error\|failed" ~/.local/share/xorg/Xorg.0.log'
+alias dns="~/.config/bspwm/dns.sh"
+alias zapret="~/.config/bspwm/zapret.sh"
+
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+#  echo -n "Welcome to the "
+#  set_color yellow
+#  echo -n "Los Pollos "
+#  set_color normal
+#  echo -n "Hermanos famaly, my name is "
+#  set_color red
+#  echo -n "Gustavo"
+#  set_color normal
+#  echo -n ", but you can call me "
+#  set_color green
+#  echo -n "Gus!"
+end
